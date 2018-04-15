88da7ee87c57b27253044bc948cd6a3644413634
Xianan Li <xiananli727@163.com>
2018-04-15 14:26:40 -0700

    Update sender.py

--- sender.py
+++ sender.py
@@ -1,1 +1,11 @@
 
+/user/bin/env python
+
+import pika;
+
+url = 'amqp://axqoqmag:F6YYV-urFsNOap_GsAar2g-9ZrQ9RSZO@otter.rmq.cloudamqp.com/axqoqmag'
+params = pika.URLParameters(url)
+connection = pika.BlockingConnection(params)
+channel = connection.channel()
+channel.basic_publish(exchange='', routing_key='hello', body='Hello Viewers!')
+
